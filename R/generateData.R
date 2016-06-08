aaceca1e1509e79e589d6634adb785357f4fa970
gitronald <gitronald@users.noreply.github.com>
2016-06-08 15:21:20 -0700

    Add data generating functions

--- R/generateData.R
+++ R/generateData.R
@@ -12,11 +12,11 @@
 #' generate_dates("2016-06-05", 31, by = "day")
 #'
 generate_dates = function(startDate, nDays, by = "day") {
-  
-  startDate = as.Date(startDate)
-  nDays = nDays
-  data1 = seq(startDate, by = by, length.out = nDays)
-  return(data1)
+
+startDate = as.Date(startDate)
+nDays = nDays
+data1 = seq(startDate, by = by, length.out = nDays)
+return(data1)
 }
 
 
@@ -33,11 +33,11 @@ generate_dates = function(startDate, nDays, by = "day") {
 #'
 #' @examples
 generate_behaviors = function(nrow, nBehaviors,
-                              prStart = 0.1, prEnd = 0.3, prBy = 0.001){
-  
-  pr = sample(seq(prStart, prEnd, prBy), 
-              nBehaviors, 
-              replace = T)
-  data1 = sapply(pr, function(x) rbinom(nrow, 1, x))
-  return(data1)
+                            prStart = 0.1, prEnd = 0.3, prBy = 0.001){
+
+pr = sample(seq(prStart, prEnd, prBy), 
+            nBehaviors, 
+            replace = T)
+data1 = sapply(pr, function(x) rbinom(nrow, 1, x))
+return(data1)
 }
